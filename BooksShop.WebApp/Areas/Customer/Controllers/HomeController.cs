122037d61963fd1cbd1928da3b2aa6acf6937f40
Quang Man <manquang39@gmail.com>
2024-03-14 10:11:40 +0700

    add details page

--- BooksShop.WebApp/Areas/Customer/Controllers/HomeController.cs
+++ BooksShop.WebApp/Areas/Customer/Controllers/HomeController.cs
@@ -1,16 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BooksShop.DataAccess.Repository.IReponsitory;
+using BooksShop.Models.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BooksShop.WebApp.Areas.Customer.Controllers
 {
     [Area("Customer")]
     public class HomeController : Controller
     {
-        
+        private readonly IUnitOfWork _unitOfWork;
+
+        public HomeController(IUnitOfWork unitOfWork) 
+        {
+            _unitOfWork = unitOfWork;
+        }
         public IActionResult Index()
         {
-
-           
-            return View();
+            IEnumerable<Product> products = _unitOfWork.Product.GetAll("category");
+            return View(products);
+        }
+        public IActionResult Details(int id)
+        {
+            Product product = _unitOfWork.Product.Get(temp => temp.Id==id, "category");
+            return View(product);
         }
     }
 }
